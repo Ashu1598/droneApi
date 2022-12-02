b4153505a152c68d1f72efe71191b9540c589ac8
Amrithraj87 <amrithraj.nalam@motivitylabs.com>
2022-12-02 11:55:23 +0530

    backend updated

--- app1.0.0.js
+++ app1.0.0.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const bodyparser = require('body-parser')
-const app = express();
-const router = require('./Api/Routes/User.routes')
+const express = require("express")
+const cors = require("cors")
+const bodyparser = require("body-parser")
+const app = express()
+const router = require("./Api/Routes/User.routes")
+const config = require("./config/config")
 
-app.use(cors({
-  origin: "*"
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+)
 app.use(bodyparser.json())
 app.use("/", router)
 app.listen(config.port, () => {
-  console.log(`Example app listening at http://localhost:${config.port}`);
-});
+  console.log(`Example app listening at http://localhost:${config.port}`)
+})
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
--- config/config.js
+++ config/config.js
@@ -1,17 +1,15 @@
-
-const mysql = require('mysql')
-module.exports.port = 8000
+const mysql = require("mysql")
+module.exports.port = 8001
 
 module.exports.dbConfig = mysql.createConnection({
-    host:'localhost',
-    port:'3306',
-    user:"root",
-    password:"root"
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "root",
 })
 
 module.exports.otpConfig = {
-    // accountSid : 'AC2890ec3646d3700c9bf55c4780efb596',
-    // authToken : '82a0d6f1aa9f808634986e808dae4461',
-    // SSID : 'VAafa8984dfe519345b833e014d29e1981'
+  accountSid: "AC2890ec3646d3700c9bf55c4780efb596",
+  authToken: "82a0d6f1aa9f808634986e808dae4461",
+  SSID: "VAafa8984dfe519345b833e014d29e1981",
 }
-
