c459642d33ecc1364b4696146b898830ef3bba44
Ashutosh swain <ashutosh.swain@motivitylabs.com>
2022-12-02 11:38:51 +0530

    removed some items

--- config/config.js
+++ config/config.js
@@ -10,8 +10,8 @@ module.exports.dbConfig = mysql.createConnection({
 })
 
 module.exports.otpConfig = {
-    accountSid : 'AC2890ec3646d3700c9bf55c4780efb596',
-    authToken : '82a0d6f1aa9f808634986e808dae4461',
-    SSID : 'VAafa8984dfe519345b833e014d29e1981'
+    // accountSid : 'AC2890ec3646d3700c9bf55c4780efb596',
+    // authToken : '82a0d6f1aa9f808634986e808dae4461',
+    // SSID : 'VAafa8984dfe519345b833e014d29e1981'
 }
 
